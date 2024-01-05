e87c11d5452bc761ea7a37a8e7496f4be72aea2c
Rishy-2021 <riteshpuwar2001@gmail.com>
2024-01-06 04:53:47 +0530

    add cors header

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import authRoutes from "./routes/auth-route"
+import bodyParser from "body-parser"
 
 dotenv.config();
 
@@ -11,6 +12,13 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(bodyParser.json({ limit: "500mb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "500mb" }));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
 app.use("/auth", authRoutes);
 
 mongoose.connect(`${process.env.MONGO_URI}`);
